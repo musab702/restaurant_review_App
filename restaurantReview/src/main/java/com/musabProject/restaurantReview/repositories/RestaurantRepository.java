0d3195dc7097828de7342aa346b1e1f20d68abc2
musab702 <musabshaikh702@gmail.com>
2025-04-22 00:42:37 -0400

    Added more entities and a repository for restaurant

---
+++ restaurantReview/src/main/java/com/musabProject/restaurantReview/repositories/RestaurantRepository.java
@@ -0,0 +1,11 @@
+package com.musabProject.restaurantReview.repositories;
+
+import com.musabProject.restaurantReview.domain.entities.Restaurant;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends ElasticsearchRepository<Restaurant, String> {
+
+
+}

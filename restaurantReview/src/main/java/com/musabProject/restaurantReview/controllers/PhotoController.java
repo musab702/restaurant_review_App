7f9c1ba73042f73ff66508ce613f4267c5fff64e
musab702 <musabshaikh702@gmail.com>
2025-04-23 16:22:16 -0400

    added photo retrieval endpoint

--- restaurantReview/src/main/java/com/musabProject/restaurantReview/controllers/PhotoController.java
+++ restaurantReview/src/main/java/com/musabProject/restaurantReview/controllers/PhotoController.java
@@ -4,11 +4,13 @@ import com.musabProject.restaurantReview.domain.dtos.PhotoDto;
 import com.musabProject.restaurantReview.domain.entities.Photo;
 import com.musabProject.restaurantReview.mappers.PhotoMapper;
 import com.musabProject.restaurantReview.services.PhotoService;
+import org.springframework.core.io.Resource;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -23,5 +25,17 @@ public class PhotoController {
     public PhotoDto uploadPhoto(@RequestParam("file")MultipartFile file){
         Photo savedPhoto = photoService.uploadPhoto(file);
         return photoMapper.toDto(savedPhoto);
+    }
+
+    @GetMapping(path = "/{id:.+}")
+    public ResponseEntity<Resource> getPhoto(@PathVariable String id){
+        return photoService.getPhotoAsResource(id).map(photo ->
+                ResponseEntity.ok()
+                        .contentType(MediaTypeFactory.getMediaType(photo)
+                                .orElse(MediaType.APPLICATION_OCTET_STREAM)
+                        )
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "inline")
+                        .body(photo)
+        ).orElse(ResponseEntity.notFound().build());
     }
 }
